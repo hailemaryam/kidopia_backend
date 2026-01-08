422b7b2a510c3c5f92ab11b7037f2e3c8f7330c3
hailemaryam <hailemaryammecca@gmail.com>
2026-01-08 12:27:44 +0300

    point add interval 30 min changed to 5 min

--- game_portal/game_portal/api.py
+++ game_portal/game_portal/api.py
@@ -252,8 +252,8 @@ def addPoint():
     subscription = get_subscription_by_phone(phone_number)
     if not subscription:
         frappe.throw("Subscription not found")
-    if subscription.last_point_added and subscription.last_point_added > datetime.now() - timedelta(minutes=30):
-        frappe.throw("You can only add point once every 30 minutes.")
+    if subscription.last_point_added and subscription.last_point_added > datetime.now() - timedelta(minutes=5):
+        frappe.throw("You can only add point once every 5 minutes.")
     subscription.point += point
     subscription.last_point_added = datetime.now()
     subscription.save(ignore_permissions=True)
