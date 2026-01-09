77a21d91e9e6e5d67799ab50baad9d6ece6eb2b7
hailemaryam <hailemaryammecca@gmail.com>
2026-01-09 16:44:33 +0300

    clear point functionality added

--- game_portal/game_portal/doctype/leader_board_setting/leader_board_setting.js
+++ game_portal/game_portal/doctype/leader_board_setting/leader_board_setting.js
@@ -1,8 +1,45 @@
 // Copyright (c) 2025, hailemaryam and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Leader Board Setting", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Leader Board Setting", {
+	refresh(frm) {
+		// Add click handler for Clear Point button
+		if (frm.fields_dict.clear_points_button) {
+			frm.fields_dict.clear_points_button.$input.on("click", function() {
+				// Show confirmation dialog
+				frappe.confirm(
+					__("Are you sure you want to clear all subscription points? This action cannot be undone."),
+					function() {
+						// User confirmed
+						frappe.call({
+							method: "game_portal.game_portal.doctype.leader_board_setting.leader_board_setting.clear_all_points",
+							callback: function(r) {
+								if (r.message && r.message.success) {
+									frappe.show_alert({
+										message: __(r.message.message),
+										indicator: "green"
+									}, 5);
+									frm.reload_doc();
+								} else {
+									frappe.show_alert({
+										message: __(r.message.message || "Failed to clear points"),
+										indicator: "red"
+									}, 5);
+								}
+							},
+							error: function(r) {
+								frappe.show_alert({
+									message: __("An error occurred while clearing points"),
+									indicator: "red"
+								}, 5);
+							}
+						});
+					},
+					function() {
+						// User cancelled - do nothing
+					}
+				);
+			});
+		}
+	},
+});
--- game_portal/game_portal/doctype/leader_board_setting/leader_board_setting.py
+++ game_portal/game_portal/doctype/leader_board_setting/leader_board_setting.py
@@ -1,9 +1,32 @@
 # Copyright (c) 2025, hailemaryam and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class LeaderBoardSetting(Document):
 	pass
+
+
+@frappe.whitelist()
+def clear_all_points():
+	"""Clear all point values in Subscription doctype to 0"""
+	try:
+		# Update all subscription records to set point = 0
+		frappe.db.sql("""
+			UPDATE `tabSubscription`
+			SET point = 0
+		""")
+		frappe.db.commit()
+		
+		return {
+			"success": True,
+			"message": "All subscription points have been cleared successfully."
+		}
+	except Exception as e:
+		frappe.log_error(frappe.get_traceback(), "Clear Points Error")
+		return {
+			"success": False,
+			"message": f"Failed to clear points: {str(e)}"
+		}
